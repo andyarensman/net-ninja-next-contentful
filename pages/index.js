cc6aac37e39edfe4b5e4770a6ea1b4356a803906
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-05-24 12:14:35 -0700

    lesson 3 - contentful client

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,23 @@
-export default function Recipes() {
+import { createClient } from 'contentful';
+
+export async function getStaticProps() {
+
+  const client = createClient({
+    space: process.env.CONTENFUL_SPACE_ID,
+    accessToken: process.env.CONTENFUL_ACCESS_KEY
+  })
+
+  const res = await client.getEntries({ content_type: 'recipe' })
+
+  return {
+    props: {
+      recipes: res.items
+    }
+  }
+
+}
+
+export default function Recipes({ recipes }) {
   return (
     <div className="recipe-list">
       Recipe List
